41b18bed2a0a6af5759e8ebc4142cb4c99cb2ee4
Vasyl-Stashchuk <65713313+Vasyl-Stashchuk@users.noreply.github.com>
2022-03-01 18:57:36 +0200

    РНБ КЛУБ

--- js/project-separate.js
+++ js/project-separate.js
@@ -1,27 +1,18 @@
+
 (function() {
-    const project = JSON.parse(localStorage.project);
-    const productsContainer = document.querySelector('.block-separate');
+
+  const project = JSON.parse(localStorage.project);
+  const productsContainer = document.querySelector('.block-separate');
     productsContainer.innerHTML = `
     <div class="top-separate">
     <h3>${project.title}</h3>
     <hr class="super-hr">
     </div>
   <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
+  <div class="carousel-inner" data-id="${project.id}">
     <div class="carousel-item active">
-      <img src="${project.image1}" class="d-block w-100" alt="${project.title}">
+      <img src="${project.image}" class="d-block w-100" alt="${project.title}">
     </div>
-    <div class="carousel-item">
-      <img src="${project.image2}" class="d-block w-100" alt="${project.title}">
-    </div>
-    <div class="carousel-item">
-      <img src="${project.image3}" class="d-block w-100" alt="${project.title}">
-    </div>
-    <div class="carousel-item">
-      <img src="${project.image4}" class="d-block w-100" alt="${project.title}">
-    </div>
-    <div class="carousel-item">
-      <img src="${project.image5}" class="d-block w-100" alt="${project.title}">
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
@@ -36,7 +27,93 @@
   <section>
         <h4>${project.text}</h4>
     </section>
-            `;
-            
-          
+            `;    
 })();
+
+  (function() {
+    const images = [
+    {
+        id: "9",
+        photo:'img/9Atem/atem-1.gif',
+    },
+    {
+        id: "9",
+        photo:'img/9Atem/atem-2.gif',
+    },
+    {
+        id: "9",
+        photo:'img/9Atem/atem-3.gif',
+    },
+    {
+        id: "9",
+        photo:'img/9Atem/atem-4.gif',
+    },
+    {
+        id: "9",
+        photo:'img/9Atem/atem-5.gif',
+    },
+    {
+      id: "8",
+      photo:'img/8Voomy/voomy-it-park-1.gif',
+    },
+    {
+      id: "8",
+      photo:'img/8Voomy/voomy-it-park-2.gif',
+    },
+    {
+      id: "8",
+      photo:'img/8Voomy/voomy-it-park-3.gif',
+    },
+    {
+      id: "8",
+      photo:'img/8Voomy/voomy-it-park-4.gif',
+    },
+    {
+      id: "8",
+      photo:'img/8Voomy/voomy-it-park-5.gif',
+    },
+    {
+      id: "8",
+      photo:'img/8Voomy/voomy-it-park-6.gif',
+    },
+    {
+      id: "8",
+      photo:'img/8Voomy/voomy-it-park-7.gif',
+    },
+    {
+      id: "8",
+      photo:'img/8Voomy/voomy-it-park-8.gif',
+    },
+    {
+      id: "8",
+      photo:'img/8Voomy/voomy-it-park-9.gif',
+    },
+    {
+      id: "8",
+      photo:'img/8Voomy/voomy-it-park-10.gif',
+    },
+    {
+      id: "8",
+      photo:'img/8Voomy/voomy-it-park-11.gif',
+    },
+    {
+      id: "8",
+      photo:'img/8Voomy/voomy-it-park-12.gif',
+    }
+  ];
+  
+  function renderImages(images) {
+    const project = JSON.parse(localStorage.project);
+    const imagesContainer = document.querySelector('.carousel-inner');
+    for(const image of images)
+    if(project.id == image.id){
+            imagesContainer.innerHTML += `
+            <div class="carousel-item" data-id="${project.id}">
+            <img  src="${image.photo}" class="d-block w-100" alt="${image.title}">
+            </div>
+            `;
+        }
+}
+
+renderImages(images);
+  })();
