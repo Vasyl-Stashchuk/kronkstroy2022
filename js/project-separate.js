72155be8ec191c6ed64c3130f3d7b82c3255a783
Vasyl-Stashchuk <65713313+Vasyl-Stashchuk@users.noreply.github.com>
2022-09-05 14:08:27 +0300

    Carousel

--- js/project-separate.js
+++ js/project-separate.js
@@ -308,11 +308,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     var button = document.getElementById('prev');
     button.onclick = function() {
-        document.getElementById('container').scrollLeft -= 256;
+        document.getElementById('container').scrollLeft -= 400;
     };
     var button = document.getElementById('next');
     button.onclick = function() {
-        document.getElementById('container').scrollLeft += 256;
+        document.getElementById('container').scrollLeft += 400;
     };
 }, false);
 
