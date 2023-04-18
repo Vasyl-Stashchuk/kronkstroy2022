5eaf29bb7c0adf8eaa124434711407689f612a1c
Vasyl-Stashchuk <65713313+Vasyl-Stashchuk@users.noreply.github.com>
2023-04-18 14:46:53 +0300

    Delete js directory

--- js/first-screen.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    const project = JSON.parse(localStorage.project);
-    const productsContainer = document.querySelector('.first-screen');
-    productsContainer.innerHTML += `
-   <div class="first-item" data-id="${project.id}" style="background-image:url(${project.image});">
-        </div>
-          `;
-})();
